5c17304c1f13a6ff24fe3da6a423b436abbbd696
James S White <james@jameswhite.org>
2011-10-05 09:50:04 -0500

    debugging guestid bug

--- lib/VMware/ESX/Actions.pm
+++ lib/VMware/ESX/Actions.pm
@@ -76,6 +76,7 @@ sub destroy{
 sub deploy{
     my $self = shift;
     my $cb = shift if @_;
+    print STDERR Data::Dumper->Dump([ 'cb' => $cb]);
     $self->do('create_vm',$cb);
     return $self;
 }
@@ -247,7 +248,7 @@ sub destroy_vm{
 sub create_vm {
    my $self = shift;
    my $args = shift if @_;
-   print STDERR Data::Dumper->Dump([ 'cb'=> $cb, 'args' => $args]);
+   print STDERR Data::Dumper->Dump([ 'args' => $args]);
    my @vm_devices;
    $self->load_env();
    my $host_view = Vim::find_entity_view(view_type => 'HostSystem',
