0bd92dc1043897a005570e4f2199f39dcdf2456b
James S White <jameswhite@eftsource.com>
2013-07-24 10:51:28 -0500

    folder_views

--- lib/VMware/ESX/Actions.pm
+++ lib/VMware/ESX/Actions.pm
@@ -340,8 +340,6 @@ sub create_vm {
        $vm_folder_view = Vim::get_view(mo_ref => $datacenter->vmFolder);
    }
 
-print Data::Dumper->Dump([ $vm_folder_view ]);
-exit 0;
    my $comp_res_view = Vim::get_view(mo_ref => $host_view->parent);
    my $respool_handle = $comp_res_view->resourcePool;
    my $respool_res_view = Vim::get_view(mo_ref => $comp_res_view->resourcePool);
